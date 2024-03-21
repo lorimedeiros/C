776c0f173def8b00d16415424f4e3efa4cacd2fc
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2024-03-21 09:06:56 -0300

    Create EstruturaCondicional.c

---
+++ Basico/EstruturaCondicional.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 5;
+
+    // Verifica se o número é maior, igual ou menor que 5
+    if (numero > 5) {
+        printf("O número é maior que 5.\n");
+    } else if (numero == 5) {
+        printf("O número é igual a 5.\n");
+    } else {
+        printf("O número é menor que 5.\n");
+    }
+
+    return 0;
+}
